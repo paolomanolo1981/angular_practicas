32b9a60c11e5878ff8cf612921b295dcd2ace76d
epecho <epecho@minsa.gob.pe>
2022-04-27 18:20:25 -0500

    d

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -3,23 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+// import { ContadorComponent } from './contador/contador/contador.component';
+// import { HeroeComponent } from './heroes/heroe/heroe.component';
+// import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
  
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent,
+    //ContadorComponent
+
+    // HeroeComponent,
+    // ListadoComponent,
   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
